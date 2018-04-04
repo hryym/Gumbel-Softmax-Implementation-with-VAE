367b0feeb9352302cfc431fb3bf57511b5d7d03d
hryym <yimingyan111@gmail.com>
2018-04-04 10:14:52 -0700

    finish train

--- cvae.py
+++ cvae.py
@@ -5,7 +5,10 @@ import torch
 from torchvision import datasets, transforms
 from torch import nn, optim
 from torch.autograd import Variable
-from torch.distributions.bernoulli import Bernoulli
+from torch.distributions import Bernoulli
+from matplotlib import pyplot as plt
+import inspect
+import ipdb
 
 parser = argparse.ArgumentParser(description = "categorical VAE with MNIST")
 parser.add_argument('--batch_size', type = int, default = 100,
@@ -31,15 +34,6 @@ parser.add_argument('--N', type = int, default = 30,
 args = parser.parse_args()
 args.cuda = torch.cuda.is_available() and args.use_cuda
 
-data_path = os.path.expanduser('~/data/mnist')
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(data_path, train = True, download = True,
-                   transform = transforms.ToTensor()),
-    batch_size = args.batch_size, shuffle = True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(data_path, train = False, transform = transforms.ToTensor()),
-    batch_size = args.batch_size, shuffle = True, **kwargs)
 
 class CVAE(nn.Module):
     def __init__(self, N, K, tau):
@@ -52,19 +46,22 @@ class CVAE(nn.Module):
         self.fc5 = nn.Linear(256, 512)
         self.fc6 = nn.Linear(512, 784)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=-1)
+        self.sigmoid = nn.Sigmoid()
         self.K = K
         self.N = N
         self.temperature = tau
 
 
     def _sample_gumbel(self, shape, eps=1e-20):
-        print('_sample_gumbel shape', shape)
         U = torch.rand(shape)
         return -torch.log(-torch.log(U + eps) + eps)
 
     def _gumbel_softmax_sample(self, logits):
-        y = logits + self._sample_gumbel(list(logits.size()[-1]))
+        sample = Variable(self._sample_gumbel(logits.size()[-1]))
+        if logits.is_cuda:
+            sample = sample.cuda()
+        y = logits + sample
         return self.softmax(y / self.temperature)
 
     def _gumbel_softmax(self, logits, hard=False):
@@ -77,39 +74,43 @@ class CVAE(nn.Module):
         h3 = self.fc3(h2)
         logits_y = h3.view(-1, self.K)
         q_y = self.softmax(logits_y)
-        return q_y
+        return q_y, logits_y
 
     def _decoder(self, y):
         h3 = self.relu(self.fc4(y))
         h4 = self.relu(self.fc5(h3))
         logits_x = self.fc6(h4)
-        p_x = Bernoulli(logits=logits_x)
-        return p_x
+        p_x = self.sigmoid(logits_x)
+        return p_x, logits_x
 
     def forward(self, x):
-        q_y = self._encoder(x)
-        y = torch.self._gumbel_softmax(q_y).view(-1, self.N * self.K)
-        p_x = _decoder(y)
-        return p_x, q_y
+        q_y, logits_y = self._encoder(x.view(-1, 784))
+        y = self._gumbel_softmax(logits_y).view(-1, self.N * self.K)
+        p_x, logits = self._decoder(y)
+        return p_x, q_y, logits
 
-def loss_fn(q_y, p_x, x, N, K):
-    kl = (q_y * (torch.log(q_y) - torch.log(1.0/K))).view(-1, N, K)
-    KL = torch.sum(torch.sum(kl, 2), 1)
-    elbo = torch.sum(p_x.log_prob(x), 1) - KL
-    loss = torch.mean(-elbo)
+def loss_fn(q_y, p_x, x, N, K, logits):
+    prior = Variable(torch.log(torch.from_numpy(np.array([1.0/K]))).type(torch.FloatTensor)).cuda()
+    kl = (q_y * (torch.log(q_y+1e-20) - prior)).view(-1, N, K)
+    KL = torch.sum(kl)
+    data = x.view(-1, 784)
+    bceloss = nn.BCELoss(size_average=False)
+    elbo = -bceloss(p_x, data) - KL
+    loss = -(1.0/p_x.size()[0])*elbo
     return loss
 
-def train(model, optimizer, num_iters, dat, args):
+def train(model, optimizer, train_loader, num_iters, dat, args):
     model.train()
     train_loss = 0
+    np_temp = model.temperature
     for i, (data, _) in enumerate(train_loader):
         num_iters += 1
         if args.cuda:
             data = data.cuda()
         data = Variable(data)
         optimizer.zero_grad()
-        p_x, q_y = model(data)
-        loss = loss_fn(q_y, p_x, data, N, K)
+        p_x, q_y, logits = model(data)
+        loss = loss_fn(q_y, p_x, data, args.N, args.K, logits)
         loss.backward()
         train_loss += loss.data[0]
         optimizer.step()
@@ -121,28 +122,59 @@ def train(model, optimizer, num_iters, dat, args):
             for param_group in optimizer.param_groups:
                 param_group['lr'] *= 0.9
             model.temperature = np_temp
+        print("loss", -loss.data[0])
+        ipdb.set_trace()
         if num_iters % args.log_interval == 1:
             print('Step %d, ELBO: %0.3f' % (num_iters,-loss.data[0]))
+
+
     return model, optimizer, num_iters, dat
 
-    def test(model):
+    def test(model, test_loader):
         model.eval()
         test_loss = 0
         for i, (data, _) in enumerate(test_loader):
             if args.cuda:
                 data = data.cuda()
             data = Variable(data, volatile=True)
-            p_x, q_y = model(data)
-            test_loss += loss_fn(q_y, p_x, data, N, K).data[0]
+            #p_x, q_y, logits = model(data)
+            #test_loss += loss_fn(q_y, p_x, data, N, K).data[0]
+
+
 
 def main(args):
+    torch.manual_seed(100)
+    if args.cuda:
+        torch.cuda.manual_seed(100)
+
+    data_path = os.path.expanduser('~/data/mnist')
+    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(data_path, train = True, download = True,
+                   transform = transforms.ToTensor()),
+        batch_size = args.batch_size, shuffle = True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(data_path, train = False, transform = transforms.ToTensor()),
+        batch_size = args.batch_size, shuffle = True, **kwargs)
+
     num_iters = 0
     dat = []
     model = CVAE(args.N, args.K, args.tau)
-    if args.cuda():
+    if args.cuda:
         model.cuda()
     optimizer = optim.Adam(model.parameters(), lr = args.lr)
     while num_iters <= args.num_iters:
         model, optimizer, num_iters, dat = train(model,
-            optimizer, num_iters, dat, args)
-        test(model)
+            optimizer, train_loader, num_iters, dat, args)
+        #test(model, test_loader)
+    dat=np.array(dat).T
+    f,axarr=plt.subplots(1,2)
+    axarr[0].plot(dat[0],dat[1])
+    axarr[0].set_ylabel('Temperature')
+    axarr[1].plot(dat[0],dat[2])
+    axarr[1].set_ylabel('-ELBO')
+    plt.show()
+
+if __name__ == '__main__':
+    with ipdb.slaunch_ipdb_on_exception():
+        main(args)
